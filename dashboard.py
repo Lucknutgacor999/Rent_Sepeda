0d6560172b71c9379797bb974fd3e71435a1c8c0
Lucknutgacor999 <150572348+Lucknutgacor999@users.noreply.github.com>
2025-03-14 01:45:01 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,7 @@ df = pd.read_csv("data_clean.csv")
 
 # Sidebar
 st.sidebar.title("Bicycle Rent")
-st.sidebar.image ("C:\\Users\\acer\\Submission\\img\\logo.jpg", width=300) 
+st.sidebar.image("img/logo.jpg", width=300)
 
 # Title
 st.title("📊 Dashboard Penyewaan Sepeda")
